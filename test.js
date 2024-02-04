4b6a0fbabc8b13b4288534fd1b08227006b7bc9a
Andrei Prepodobniy <kohlschreiber@mail.ru>
2024-02-04 21:19:00 +0200

    add ainput1-4 sitetwo

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 document.getElementById("test").addEventListener("click", function(e){
     e.preventDefault
 
-    console.log("WoW")
+    let a = Number(document.getElementById("input1").value);
+    let b = Number(document.getElementById("input2").value);
+    let c = Number(document.getElementById("input3").value);
+    let f = Number(document.getElementById("input4").value);
+    let sum = a + b + c + f
+    if (
+        a < 0 || b < 0 || c < 0 || f < 0 
+    ) {
+        alert("Error login")
+    } else {
+        alert(sum)
+    }
 })
